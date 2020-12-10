869e63e368ede4553d74bac385a9f4b9d361100d
rezamax <reza.max555@gmail.com>
2020-12-10 20:22:46 +0330

    Add ChatRoom and  Write msg

--- main.go
+++ main.go
@@ -19,6 +19,22 @@ type Client struct {
 	connection *websocket.Conn
 }
 
+type ChatRoom struct {
+	clients []*Client
+}
+
+func (chatroom *ChatRoom) Join(name int, conn *websocket.Conn) {
+	client := NewClient(rand.Intn(100), conn)
+	chatroom.clients = append(chatroom.clients, client)
+}
+
+func NewChatRoom() *ChatRoom {
+	chatroom := &ChatRoom{
+		clients: make([]*Client, 0),
+	}
+	return chatroom
+}
+
 func NewClient(name int, conn *websocket.Conn) *Client {
 	client := &Client{
 		name:       name,
@@ -42,12 +58,17 @@ func wsServer(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err)
 	}
+
+	chatroom := NewChatRoom()
+	chatroom.Join(rand.Intn(100), ws)
+
 	client := NewClient(rand.Intn(100), ws)
 	fmt.Println(client)
-	client.Reader()
+	client.Reader(chatroom)
+
 }
 
-func (client *Client) Reader() {
+func (client *Client) Reader(chatroom *ChatRoom) {
 	for {
 		_, p, err := client.connection.ReadMessage()
 		if err != nil {
@@ -55,12 +76,16 @@ func (client *Client) Reader() {
 			log.Println(err)
 			return
 		}
-		fmt.Printf("user --> %d:%s \n", client.name, string(p))
+		fmt.Printf("user_%d:%s \n", client.name, string(p))
+		for _, c := range chatroom.clients {
+			c.Writer(string(p), client.name)
+		}
 	}
 }
 
-func writer() {
-
+func (client *Client) Writer(msg string, writer int) {
+	wn := fmt.Sprintf("user_%d : %s", writer, msg)
+	client.connection.WriteMessage(1, []byte(wn))
 }
 
 func router() {
